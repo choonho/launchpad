1d04faf072096d101102bc14c9873b89a4f9642b
Choonho Son <choonhoson@megazone.com>
2024-01-10 20:53:46 +0900

    Update for latest terraform (Issue #6)
    terraform-aws-modules/terraform-aws-vpc (2.77.0 -> 5.5.0)
    terraform-aws-modules/terraform-aws-eks (18.21.0 -> 19.21.0)
    
    Signed-off-by: Choonho Son <choonhoson@megazone.com>

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -10,7 +10,7 @@
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"  # Source Verion
+  version = "5.5.0"  # Source Version (2024-01-09)
 
   # VPC
   name = var.vpc_name
@@ -19,11 +19,17 @@ module "vpc" {
   # AZ
   azs               = ["${var.region}a", "${var.region}b", "${var.region}c"]
   private_subnets   = var.private_subnets
-  database_subnets  = var.database_subnets
   
   # NAT(for Internet)
+  # (Option 1) Single NAT GW (for DEV)
+  #enable_nat_gateway = true
+  #public_subnets     = var.public_subnets
+
+  # (Option 2) One NAT GW per AZ (for PROD)
+  enable_nat_gateway    = true
+  single_nat_gateway 	= false
+  one_nat_gateway_per_az = true
   public_subnets     = var.public_subnets
-  enable_nat_gateway = true
 
   # Disable VPN
   enable_vpn_gateway = false
@@ -33,7 +39,6 @@ module "vpc" {
   enable_dns_support   = true
 
   tags     = var.tags
-  vpc_tags = var.vpc_tags
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                    = "1"
@@ -47,13 +52,25 @@ module "vpc" {
 # EKS cluster
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "18.21.0"
+  version         = "19.21.0" # Source Version (2023-12-11)
 
   cluster_name    = var.cluster_name
   cluster_version = var.eks_cluster_version
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
   tags            = var.tags
+
+  cluster_addons = {
+    vpc-cni = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    coredns = {
+      most_recent = true
+    }
+  }
 
   # Extend cluster security group rules
   cluster_security_group_additional_rules = {
@@ -67,34 +84,34 @@ module "eks" {
     }
   }
 
-  # Extend node-to-node security group rules
-  node_security_group_additional_rules = {
-    ingress_allow_access_from_control_plane = {
-      type                          = "ingress"
-      protocol                      = "tcp"
-      from_port                     = 9443
-      to_port                       = 9443
-      source_cluster_security_group = true
-      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-    }
-    ingress_self_all = {
-      description = "Node to node all ports/protocols"
-      protocol    = "-1"
-      from_port   = 0
-      to_port     = 0
-      type        = "ingress"
-      self        = true
-    }
-    egress_all = {
-      description      = "Node all egress"
-      protocol         = "-1"
-      from_port        = 0
-      to_port          = 0
-      type             = "egress"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  }
+#  # Extend node-to-node security group rules
+#  node_security_group_additional_rules = {
+#    ingress_allow_access_from_control_plane = {
+#      type                          = "ingress"
+#      protocol                      = "tcp"
+#      from_port                     = 9443
+#      to_port                       = 9443
+#      source_cluster_security_group = true
+#      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+#    }
+#    ingress_self_all = {
+#      description = "Node to node all ports/protocols"
+#      protocol    = "-1"
+#      from_port   = 0
+#      to_port     = 0
+#      type        = "ingress"
+#      self        = true
+#    }
+#    egress_all = {
+#      description      = "Node all egress"
+#      protocol         = "-1"
+#      from_port        = 0
+#      to_port          = 0
+#      type             = "egress"
+#      cidr_blocks      = ["0.0.0.0/0"]
+#      ipv6_cidr_blocks = ["::/0"]
+#    }
+#  }
 
   cluster_endpoint_private_access = var.cluster_endpoint_private_access
   cluster_endpoint_public_access  = var.cluster_endpoint_public_access
